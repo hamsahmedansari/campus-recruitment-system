c1ac3450599f5a919a8fef92c51c5821e08ed892
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-13 20:11:28 +0500

    Update: :construction: API Routes with MongoDB Auth  4 is Modified :  1 : deleted:server/routes/auth/admin.js  2 : deleted:server/routes/auth/company.js  3 : server/routes/auth/index.js  4 : deleted:server/routes/auth/student.js  1 is Created :  1 : server/routes/auth/users.js

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,16 +1,6 @@
 const router = require("express").Router();
-const admin = require("./admin");
-const student = require("./student");
-const company = require("./company");
+const users = require("./users");
 
-router.get("/", (req, res) => {
-  res.status(404).json({
-    message: "Nothing to GET"
-  });
-});
-
-router.use("/admin", admin);
-router.use("/student", student);
-router.use("/company", company);
+router.use("/", users);
 
 module.exports = router;
---
+++ server/routes/auth/users.js
@@ -0,0 +1,125 @@
+const router = require("express").Router();
+
+const { isInObject } = require("../../helpers");
+const { Users } = require("../../models/users");
+
+router.get("/", async (req, res) => {
+  try {
+    let data = await Users.find();
+    if (!data)
+      return res.status(404).json({
+        message: "User Not Found"
+      });
+
+    const newData = data.map(d => {
+      const temp = { ...d._doc };
+      delete temp.password;
+      return temp;
+    });
+    res.status(200).json({
+      message: "Successfully Updated",
+      newData
+    });
+  } catch (error) {}
+});
+
+router.get("/:id", async (req, res) => {
+  let { id } = req.params;
+  try {
+    let data = await Users.findById(id);
+    if (!data)
+      return res.status(404).json({
+        message: "User Not Found"
+      });
+
+    res.status(200).json({
+      message: "Successfully Updated",
+      data
+    });
+  } catch (error) {}
+});
+
+router.post("/", async (req, res) => {
+  const validate = isInObject(req.body, [
+    "userName",
+    "email",
+    "password",
+    "accountType"
+  ]);
+  if (typeof validate === "string") {
+    return res.status(400).json({
+      message: "Got Error in the Form",
+      data: validate
+    });
+  }
+
+  let obj = new Users({
+    userName: req.body.userName,
+    email: req.body.email,
+    password: req.body.password,
+    accountType: req.body.accountType
+  });
+  try {
+    let data = await obj.save();
+    res.status(200).json({
+      message: "Successfully Created Added",
+      data
+    });
+  } catch (errors) {
+    return res.status(400).json({
+      message: "Got Error in the Form",
+      data: errors.errmsg
+    });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  let { id } = req.params;
+  try {
+    let users = await Users.findById(id);
+    if (!users)
+      return res.status(404).json({
+        message: "User Not Found"
+      });
+    users.email = "email" in req.body ? req.body.email : users.email;
+    users.userName =
+      "userName" in req.body ? req.body.userName : users.userName;
+    users.password =
+      "password" in req.body ? req.body.password : users.password;
+
+    const data = await users.save();
+
+    res.status(200).json({
+      message: "Successfully Updated",
+      data
+    });
+  } catch (error) {}
+});
+
+router.delete("/:id", async (req, res) => {
+  let { id } = req.params;
+  try {
+    let user = await Users.findById(id);
+    if (!user)
+      return res.status(404).json({
+        message: "User Not Found "
+      });
+    const result = await Users.findByIdAndRemove(id);
+    if (result) {
+      res.status(200).json({
+        message: "Successfully Deleted",
+        data: result
+      });
+    } else {
+      res.status(500).json({
+        message: "Server Error"
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: "Todo Not Found with Bad Request"
+    });
+  }
+});
+
+module.exports = router;
