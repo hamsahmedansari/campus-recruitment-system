f8062490586943ea1396aa7ee6ee51190c7f5d89
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-13 13:12:09 +0500

    Update: :construction: projects setup  6 is Created :  1 : app.js  2 : package-lock.json  3 : package.json  4 : yarn.lock  5 :  6 : nothingaddedtocommitbutuntrackedfilespresent(usegitaddtotrack

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(404).json({
+    message: "Page Not Found"
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App is running on Port ${port}`));
