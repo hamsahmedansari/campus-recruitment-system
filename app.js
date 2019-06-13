b57a78999d003754b0376bba90bc9880094a8c29
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-13 13:27:26 +0500

    Update: :construction: API 404  3 is Modified :  1 : app.js  2 : server/routes/index.js  3 : server/routes/students/index.js

--- app.js
+++ app.js
@@ -14,5 +14,12 @@ app.get("/", (req, res) => {
 // APIS Routes
 app.use("/api/", api);
 
+// 404 NOT FOUND
+app.get("*", (req, res) => {
+  res.status(404).json({
+    message: "Page Not Found"
+  });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App is running on Port ${port}`));
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
+// Api Students
+const students = require("./students");
 
 router.get("/", (req, res) => {
   res.status(200).json({
-    message: "Api Start Point",
-    data: null
+    message: "Welcome to Campus Recruitment System"
   });
 });
+
+router.use("/students", students);
 
 module.exports = router;
